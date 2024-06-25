4abe401f66b84c4a127e8cdb173dfa5ba37d4e42
lucian.filote <lucian.filote@airalo.com>
2024-06-25 22:07:55 +0300

    revert nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    ['@nuxtjs/dotenv', { path: './' }]
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
